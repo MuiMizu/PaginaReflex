f109a784982f28ddf6168adc49575d90dd099401
Fabian <hermanoslorenzop2@gmail.com>
2025-05-25 12:11:37 -0400

    Cambio de fondo y edicion de detalles

--- PaginaReflex/PaginaReflex.py
+++ PaginaReflex/PaginaReflex.py
@@ -3,7 +3,7 @@ import reflex as rx
 def barra():
     return rx.hstack(
         rx.hstack(
-            rx.text("A.N.E.M.O", icon_class_name="coin", font_size="1.5rem", font_weight="bold", color="white", spacing="1",
+            rx.text("A.N.E.M.O", font_size="1.5rem", font_weight="bold", color="white", spacing="1",
             align="center",),
         ),
         rx.hstack(
@@ -15,18 +15,18 @@ def barra():
             rx.text("Pricing", color="white", cursor="pointer"),
 
             rx.text("For Accountants", color="white", cursor="pointer"),
-            spacing="4", align="center",
+            spacing="8", margin="5", align="center",
         ),
 
         rx.hstack(
-            rx.button("Log in", variant="ghost", color="white",
+            rx.button("Log in", background_color="transparent", border_color="white", cursor="pointer",
                       _hover={"background" : "rgba(255,255,255,0.1)"},),
 
-            rx.button("Lets Go", background="white", color="black", border_radius="0.5rem",
+            rx.button("Lets Go", background="grey", color="white", border_radius="0.5rem",
                       padding="0.75rem 1.5rem", font_weight="500",
                       _hover={"background": "rgba(255,255,255,0.9)"}),
 
-            spacing="2",
+            spacing="5",
             align="center",
         ),
 
@@ -42,12 +42,13 @@ def barra():
     )
 
 def principal():
-    return rx.box(  # ← CAMBIO DE container A box
+    return rx.box( 
         rx.hstack(
             rx.vstack(
                 rx.heading(
                     "Every Step",
-                    rx.text("Worth Gold.", display="block"),
+                    rx.text("Worth Gold.", margin_top="1rem"),
+                    spacing="4",
                     font_size="4rem",
                     font_weight="bold",
                     color="white",
@@ -57,12 +58,12 @@ def principal():
 
                 rx.text(
                     "Work with the best tools to make your work easier and more efficient.",
-                    font_size="1.2rem",
+                    font_size="1.3rem",
                     font_weight="bold",
                     color="white",
                     line_height="1.6",
                     margin_bottom="1rem",
-                    max_width="500px",
+                    max_width="430px",
                 ),
 
                 rx.hstack(
@@ -75,7 +76,8 @@ def principal():
                               padding="1rem 2rem", font_size="1rem", background="transparent",
                               _hover={"background": "rgba(255,255,255,0.1)"}),
 
-                    spacing="2",
+                    spacing="3",
+
                 ),
                 spacing="0",
             ),
@@ -85,8 +87,7 @@ def principal():
             justify="start",
             width="100%",
             padding="4rem 2rem",
-            spacing="6",
-            margin_left="7rem",
+            margin_left="2rem",
             margin_bottom="8rem",
         ),
         width="100%",
